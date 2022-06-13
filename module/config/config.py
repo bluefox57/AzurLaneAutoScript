be99af3108b316251164ea1a79952991c0768dbd
bluefox57 <76176720+bluefox57@users.noreply.github.com>
2022-06-13 19:52:36 -0400

    Opt: Restore Enable Commission, Research, and Reward

--- module/config/config.py
+++ module/config/config.py
@@ -123,7 +123,7 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
 
     def load(self):
         self.data = self.read_file(self.config_name)
-        self.config_override()
+        # self.config_override()
 
         for path, value in self.modified.items():
             deep_set(self.data, keys=path, value=value)
@@ -275,7 +275,7 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
 
     def update(self):
         self.load()
-        self.config_override()
+        # self.config_override()
         self.bind(self.task)
         self.save()
 
